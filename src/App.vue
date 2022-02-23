6763735f34a85f1dc0fa2129923b5138396b3d56
Debora Jaimes <deboranjaimes@gmail.com>
2022-02-23 09:44:11 -0400

    Commit para ajustar o nome do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="py-5 px-40 bg-indigo-600 shadow-md">
-    <h1 class="font-bold text-white">Solft List</h1>
+    <h1 class="font-bold text-white">Soft List</h1>
   </nav>
   <router-view></router-view>
 </template>
